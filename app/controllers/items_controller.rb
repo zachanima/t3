7f58f63df4395b0595e42d217e0e03beb33cd7e2
Zach Ewert <zachanima@gmail.com>
2012-05-05 07:48:21 +0200

    Add items controller, index and new view

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,18 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params[:item])
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   attr_accessible :buy, :name, :sell, :type_id
+  default_scope order(:name)
+  validates_presence_of :name
+  validates_numericality_of :type_id
 end
