9f4fb9b4bf2efcd194efcf5675b69047c530125e
Zach Ewert <zachanima@gmail.com>
2012-05-05 07:55:51 +0200

    Update items buy, sell values from eve-central

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ActiveRecord::Base
   attr_accessible :buy, :name, :sell, :type_id
-  default_scope order(:name)
-  validates_presence_of :name
+
+  validates :name, presence: true, uniqueness: true
   validates_numericality_of :type_id
+
+  default_scope order(:name)
+  scope :outdated, where('updated_at < ? or buy is null or sell is null',
+                         Time.now - 6.hours).limit(100)
 end
