493d2889ce3a63c1336267147693b74d881c6ba4
Zach Ewert <zachanima@gmail.com>
2012-05-05 16:25:11 +0200

    Move update values to blueprints controller

--- app/controllers/blueprints_controller.rb
+++ app/controllers/blueprints_controller.rb
@@ -1,5 +1,6 @@
 class BlueprintsController < ApplicationController
   def index
+    update_values
     @blueprints = Blueprint.all
   end
 
@@ -13,6 +14,36 @@ class BlueprintsController < ApplicationController
       redirect_to blueprints_path
     else
       render :new
+    end
+  end
+
+private
+  def update_values
+    if (items = Item.outdated.collect(&:type_id)).empty?
+      return
+    end
+
+    url = [
+      'http://api.eve-central.com/api/marketstat?typeid=',
+      items.join('&typeid='), '&usesystem=30000142'
+    ].join
+
+    sell = Array.new
+    buy = Array.new
+    data = Net::HTTP.get_response(URI.parse(url)).body
+    elements = REXML::Document.new(data).elements
+    elements.each('/evec_api/marketstat/type/sell/min') do |item|
+      sell << item.to_s.gsub(/[^0-9\.]/, '').to_f
+    end
+    elements.each('/evec_api/marketstat/type/buy/max') do |item|
+      buy << item.to_s.gsub(/[^0-9\.]/, '').to_f
+    end
+
+    sell.each_index do |i|
+      item = Item.find_by_type_id(items[i])
+      item.sell = sell[i]
+      item.buy = buy[i]
+      item.save
     end
   end
 end
