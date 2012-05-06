25f2999917e6e67c5d964fca0d0a68d7e25ad533
Zach Ewert <zachanima@gmail.com>
2012-05-06 12:39:32 +0200

    Do not multiply per run by 4 on hulls

--- app/models/blueprint.rb
+++ app/models/blueprint.rb
@@ -13,31 +13,33 @@ class Blueprint < ActiveRecord::Base
     [self.item.name, 'Blueprint'] * ' '
   end
 
+  def subsystem?
+    not %w[Legion Loki Proteus Tengu].include? self.item.name
+  end
+
   def profit_in_percent
     (self.profit * 100 / self.buy).round
   end
 
   def intact_per_run
-      (self.relic.intact.buy + self.relic.tools_buy + self.decryptor.buy) /
-      (20 * 0.84 * 0.25)
+    (self.relic.intact.buy + self.relic.tools_buy + self.decryptor.buy) / (20 * 0.84)
   end
 
   def malfunctioning_per_run
-      (self.relic.malfunctioning.buy + self.relic.tools_buy + self.decryptor.buy) /
-      (10 * 0.63 * 0.25)
+    (self.relic.malfunctioning.buy + self.relic.tools_buy + self.decryptor.buy) / (10 * 0.63)
   end
 
   def wrecked_per_run
-      (self.relic.wrecked.buy + self.relic.tools_buy + self.decryptor.buy) /
-      (3 * 0.42 * 0.25)
+    (self.relic.wrecked.buy + self.relic.tools_buy + self.decryptor.buy) / (3 * 0.42)
   end
 
   def per_run
-    [self.intact_per_run, self.malfunctioning_per_run, self.wrecked_per_run].min
+    [self.intact_per_run, self.malfunctioning_per_run, self.wrecked_per_run].min *
+      (self.subsystem? ? 4 : 1)
   end
 
   def cheapest_relic
-    if per_run == intact_per_run
+    if per_run == intact_per_run * (self.subsystem? ? 4 : 1)
       'intact'
     elsif per_run == malfunctioning_per_run
       'malfunctioning'
