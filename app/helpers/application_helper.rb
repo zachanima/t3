84dadb0bb9a2de14155e17a69c6e845704f7e5ea
Zach Ewert <zachanima@gmail.com>
2012-05-10 02:38:54 +0200

    Color item sell of buy in percent red if too high

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def color_span_tag value, text, threshold
+    if threshold >= 0 then
+      content_tag :span, class: (value >= threshold ? 'profit' : 'loss') do
+        [value, text].join
+      end
+    else
+      content_tag :span, class: (value >= -threshold ? 'loss' : nil) do
+        [value, text].join
+      end
+    end
+  end
 end
